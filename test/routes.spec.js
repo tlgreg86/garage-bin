3fcc4032085970529806622306b1c60c36271cbb
Travis <tlgreg86@gmail.com>
2017-09-14 16:36:28 -0600

    Add happy and sad path tests for client routes

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -7,3 +7,24 @@ const db = require('knex')(configuration);
 const should = chai.should();
 
 chai.use(chaiHttp);
+
+describe('Client Routes', () => {
+  it('should render homepage', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+      })
+  })
+  
+  it('should return 404 for a route that does not exist', (done) => {
+    chai.request(server)
+      .get('/api/v1/stuff')
+      .end((err, res) => {
+        res.should.have.status(404);
+        done();
+      });
+  });
+})
